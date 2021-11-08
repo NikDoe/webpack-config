8ac2bb9128f684880ecee92dba450703e80ccf9a
NikDoe <nikd0e@yandex.ru>
2021-11-08 13:38:23 +0300

    feature(webpack): mode setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require('path');
 
+let mode = "development";
+
+if(process.env.NODE_ENV === "production") {
+    mode = "production";
+}
+
 module.exports = {
-    mode: "development",
+    mode: mode,
 
     module: {
         rules: [
@@ -18,5 +24,6 @@ module.exports = {
         static: {
             directory: path.join(__dirname, './dist')
         }
-    }
+    },
+    devtool: "source-map"
 }
