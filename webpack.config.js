05977066b7c628a8db7c5cd409a05f94c639d7e3
NikDoe <nikd0e@yandex.ru>
2021-11-08 13:07:54 +0300

    feature(webpack): babel setup

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,17 @@ const path = require('path');
 module.exports = {
     mode: "development",
 
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                }
+            }
+        ]
+    },
     devServer: {
         static: {
             directory: path.join(__dirname, './dist')
